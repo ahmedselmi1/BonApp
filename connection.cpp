a807eb84ab036af3a83693ce936adac01b7fc2a9
Iyedbhd <80423170+IyedBHDEsprit@users.noreply.github.com>
2021-05-20 00:20:32 +0200

    final

--- connection.cpp
+++ connection.cpp
@@ -8,12 +8,13 @@ Connection::Connection()
 bool Connection::createconnect()
 {bool test=false;
 db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("Source_Projet2A");
+/*db.setDatabaseName("Source_Projet2A");
 db.setUserName("ahmed");
-db.setPassword("esprit20");
-//db.setDatabaseName("testdb1");//inserer le nom de la source de données ODBC
-//db.setUserName("iyed");//inserer nom de l'utilisateur
-//db.setPassword("daha");//inserer mot de passe de cet utilisateur
+db.setPassword("esprit20");*/
+
+db.setDatabaseName("testdb1");//inserer le nom de la source de données ODBC
+db.setUserName("iyed");//inserer nom de l'utilisateur
+db.setPassword("daha");//inserer mot de passe de cet utilisateur
 
 if (db.open())
 test=true;
